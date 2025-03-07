4a7f8ed4b18ff32dcccf7962af478a97e0f481af
sgsultana <sgsultana@gmail.com>
2025-03-07 10:57:56 -0800

    Delete model2 example.cpp

--- model2 example.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-double PrecipitationCalculator::ComputeMonthlyAveragePrecipitation(int month, double monthPrecipitation, int days[], double recordMonthPrecipitation[]){
-	double monthAverage = monthPrecipitation/days[month];
-	if( monthAverage > recordMonthPrecipitation[month] ){
-		std::cout << "New monthly record was observed in month" << month << std::endl;
-		recordMonthPrecipitation[month] = monthAverage;
-	}
-	return (monthAverage);
